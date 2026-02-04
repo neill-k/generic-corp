71128fef96d8d1189075b0dfe449f01c37fcce8b
Neill Killgore <nkillgore@gmail.com>
2026-02-04 01:49:26 -0600

    use sphinx

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = "Generic Corp"
+copyright = "2026, Generic Corp Team"
+author = "Generic Corp Team"
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+]
+
+# MyST parser settings (allows Markdown files)
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "tasklist",
+]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+# Theme
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "navigation_depth": 3,
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "includehidden": True,
+    "titles_only": False,
+}
+
+# General settings
+exclude_patterns = [
+    "_build",
+    "plans",
+    "solutions",
+    "ux-specs-*",
+    "CLAUDE_CODE_INTEGRATION_ARCHITECTURE.md",
+    "agent-workflow-builder.md",
+]
---
+++ docs/index.rst
@@ -0,0 +1,39 @@
+Generic Corp Documentation
+==========================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Getting Started
+
+   getting-started/quickstart
+   getting-started/installation
+   getting-started/configuration
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Architecture
+
+   architecture
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Development
+
+   api-reference
+   plugin-development
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Deployment
+
+   deployment
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contributing
+
+   contributing
+   code-of-conduct
+
+.. include:: index.md
+   :parser: myst_parser.sphinx_
